c081692a4b9ed54844ffeb31b5fb20aa17b39c40
plnvsln <mr.mightypo@gmail.com>
2022-11-18 08:13:31 +0000

    added real current data to app.js
    added current:
    - humidity
    - temperature
    - feels-like temperature
    - description;
    
    trying to solve responsivness;

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 function displayTemp(response) {
-  console.log(response.data);
+  let tempElement = document.querySelector("#current-temp");
+  tempElement.innerHTML = Math.round(response.data.temperature.current);
+  console.log(response);
+
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+
+  let descriptionElement = document.querySelector("#current-description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let tempFeelsLike = document.querySelector("#feels-like");
+  tempFeelsLike.innerHTML = Math.round(response.data.temperature.feels_like);
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
 }
 
 let apiKey = "f1089ea2a06e9tf3co914bf9c65aa287";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Chicago&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Tokyo&key=${apiKey}&units=metric`;
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemp);
