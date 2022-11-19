cb81b4f167f31805021eb33649450fe5935dce18
plnvsln <mr.mightypo@gmail.com>
2022-11-19 09:31:19 +0000

    added js day and time

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemp(response) {
+  console.log(response);
+
   let tempElement = document.querySelector("#current-temp");
   tempElement.innerHTML = Math.round(response.data.temperature.current);
-  console.log(response);
 
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = response.data.city;
@@ -14,6 +39,9 @@ function displayTemp(response) {
 
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.temperature.humidity;
+
+  let dateElement = document.querySelector("#current-date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "f1089ea2a06e9tf3co914bf9c65aa287";
